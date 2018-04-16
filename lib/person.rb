2e180648cb9932e7ea41f6e98e259b4fe3a488a0
Luis Herrera <herrera.lrh@gmail.com>
2018-04-16 23:38:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,13 @@
 class Person
-  def name= (new_name)
-    @name = new_name
-  end
 
   def name
     @name = name
   end
+
+  def name= (new_name)
+    @name = new_name
+  end
+
 
   def job
     @job = job
